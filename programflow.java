1f12f87d1a341f0967da1db63b5f99ee67beea5d
Sam Morgan <sam.morgan@mrjaffesclass.com>
2022-10-06 11:39:05 -0700

    first program

---
+++ programflow.java
@@ -0,0 +1,47 @@
+
+/**
+ * Write a description of class helloworld here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class programflow
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class helloworld
+     */
+    public programflow()
+    {
+        int a = 5;
+        int b = 2;
+        int c = 2;
+        int d = -8;
+        if (a == 5) {
+            System.out.println("SUCCESS");
+        }else{  
+            System.out.println("FAILURE");
+        }
+        if (b == 2) {
+            System.out.println("SUCCESS");
+        }else{   
+            System.out.println("FAILURE");
+        }
+        if (c == 1) {
+            System.out.println("FAILURE");
+        }else{
+            System.out.println("SUCCESS");
+        }
+        if (d == -8 && a == 4) {
+            System.out.println("SUCCESS");
+        }else{
+            System.out.println("FAILURE");
+        }
+
+        }
+    
+    }
+
+ 
